e39d620b9fc9b67c360ac908cec3851875a10dc6
fewb92 <sotojosh96@gmail.com>
2021-08-05 20:05:17 -0400

    edited test run script and added page template js

---
+++ page-template.js
@@ -0,0 +1,67 @@
+const fs = require('fs');
+const path = require('path');
+const OUTPUT_DIR = path.resolve(__dirname, "output");
+const outputPath = path.resolve( OUTPUT_DIR, "index.html" );
+
+function getEmployeeInfo( employee ) {
+    switch ( employee.getRole() ) {
+        case "Manager":
+            return `Office: ${ employee.getOfficeNumber() }`;
+        case "Engineer":
+            return `GitHub: <a href="https://github.com/${ employee.getGithub() }" target="_blank" class="card-link">${ employee.getGithub() }</a>`;
+        case "Intern":
+            return `School: ${ employee.getSchool() }`;
+    }
+}
+
+function mapEmployees( employees ) {
+    return employees
+        .map( employee => (
+            `<div class="card" style="wdith: 18rem;">
+                <div class="card-body">
+                    <h5 class="card-title">${ employee.getName() }</h5>
+                    <h6 class="card-subtitle">${ employee.getRole() }</h6>
+                    <ul list-group list-group-flush>
+                        <li class="list-group-item">Employee ID: ${ employee.getId() }</li>
+                        <li class="list-group-item">Email: <a href="mailto:${ employee.getEmail() }" class="card-link">${ employee.getEmail() }</a></li>
+                        <li class="list-group-item">${ getEmployeeInfo( employee ) }</li>
+                    </ul> 
+                </div>
+            </div>`
+        ) )
+        .join( "" );
+}
+
+function renderHTML( team ) {
+    return `<!DOCTYPE html>
+    <html lang="en">
+        <head>
+            <meta charset="UTF-8"/>
+            <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+            <meta http-equiv="X-UA-Compatible" content="ie=edge"/>
+            <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"/>
+            <link rel="stylesheet" href="./style.css"/>
+            <title>Team Profile Generator</title>
+        </head>
+        <body>
+            <header>
+                <h1>Team Profile</h1>
+            </header>
+            <main class="container">
+                ${ mapEmployees( team ) }
+            </main>
+        </body>
+    </html>`;
+}
+
+function generateHTML( team ) {
+    const HTML = renderHTML( team );
+
+    if ( !fs.existsSync( OUTPUT_DIR ) ) {
+        fs.mkdirSync( OUTPUT_DIR );
+    }
+
+    fs.writeFileSync( outputPath, HTML, "utf-8" );
+}
+
+module.exports = { generateHTML };
